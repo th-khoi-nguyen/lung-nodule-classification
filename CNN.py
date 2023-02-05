2265e211ed30e390c0e737872855b9ddefead481
th-khoi-nguyen <122146010+th-khoi-nguyen@users.noreply.github.com>
2023-02-06 02:37:31 +0700

    Create CNN.py

---
+++ CNN.py
@@ -0,0 +1,53 @@
+x_train,x_test,y_train,y_test=train_test_split(X,Y,test_size=0.25,random_state=42)
+
+# CNN model
+
+model = Sequential()
+model.add(Conv2D(filters = 32, kernel_size = (5,5),padding = 'Same',activation ='relu', input_shape = (150,150,3)))
+model.add(MaxPooling2D(pool_size=(2,2)))
+
+
+model.add(Conv2D(filters = 64, kernel_size = (3,3),padding = 'Same',activation ='relu'))
+model.add(MaxPooling2D(pool_size=(2,2), strides=(2,2)))
+ 
+
+model.add(Conv2D(filters =96, kernel_size = (3,3),padding = 'Same',activation ='relu'))
+model.add(MaxPooling2D(pool_size=(2,2), strides=(2,2)))
+
+model.add(Conv2D(filters = 96, kernel_size = (3,3),padding = 'Same',activation ='relu'))
+model.add(MaxPooling2D(pool_size=(2,2), strides=(2,2)))
+
+model.add(Flatten())
+model.add(Dense(512))
+model.add(Activation('relu'))
+model.add(Dense(5, activation = "softmax"))
+
+# augment the training data
+datagen = ImageDataGenerator(
+        featurewise_center=False,  # set input mean to 0 over the dataset
+        samplewise_center=False,  # set each sample mean to 0
+        featurewise_std_normalization=False,  # divide inputs by std of the dataset
+        samplewise_std_normalization=False,  # divide each input by its std
+        zca_whitening=False,  # apply ZCA whitening
+        rotation_range=10,  # randomly rotate images in the range (degrees, 0 to 180)
+        zoom_range = 0.1, # Randomly zoom image 
+        width_shift_range=0.2,  # randomly shift images horizontally (fraction of total width)
+        height_shift_range=0.2,  # randomly shift images vertically (fraction of total height)
+        horizontal_flip=True,  # randomly flip images
+        vertical_flip=False)  # randomly flip images
+
+datagen.fit(x_train)
+
+model.compile(optimizer=Adam(lr=0.001),loss='categorical_crossentropy',metrics=['accuracy'])
+
+History = model.fit_generator(datagen.flow(x_train,y_train, batch_size=128),
+                              epochs = 100, validation_data = (x_test,y_test),
+                              verbose = 1, steps_per_epoch=x_train.shape[0] // batch_size)
+# display the accuracy
+plt.plot(History.history['accuracy'])
+plt.plot(History.history['val_accuracy'])
+plt.title('Model Accuracy')
+plt.ylabel('Accuracy')
+plt.xlabel('Epochs')
+plt.legend(['train', 'test'])
+plt.show()
